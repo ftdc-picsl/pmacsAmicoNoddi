318ac2b252b7b5340d0b70dfa0a7b54c666edc99
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2021-06-11 17:05:00 -0400

    ENH: wrapper for amico-noddi container

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # pmacsAmicoNoddi
 Wrapper to run the AMICO implementation of NODDI on the PMACS cluster
+
+## Input
+
+DWI data in the format /path/to/dwi.[nii.gz, bvec, bval]
+Brain mask (.nii.gz)
+
+## NODDI model
+
+The model is explained in detail in the original [NODDI
+paper](https://pubmed.ncbi.nlm.nih.gov/22484410/). Briefly, given
+
+* A_t - normalized DWI signal (the data we're fitting to, normalized by the
+        average b=0 signal)
+
+NODDI fits
+
+```
+  A_t = V_{iso} * A_iso + (1 - V_{iso}) * [ V_{icv} * A_{icv} + (1 - V_{icv}) * A_{ecv} ]
+```
+where
+
+* A_{iso} - model of the isotropic signal
+* A_{icv} - model of the intracellular signal
+* A_{ecv} - model of the extracellular signal
+* V_{icv} - intracellular volume fraction (output as FIT_ICVF)
+* V_{iso} - isotropic volume fraction (output as FIT_ISOVF)
+
+The other outputs discussed below, FIT_OD and FIT_dir, are additional parameters
+used to model A_{icv} and A_{ecv}.
+
+## Output
+
+NODDI metrics computed via AMICO, and a pickle file produced by AMICO:
+
+FIT_ICVF.nii.gz - Relative intracellular volume fraction. Also called neurite
+density. It reflects the fraction of the non-isotropic signal that is estimated
+to be from intracellular diffusion inside neurites.
+
+FIT_OD.nii.gz - Neurite orientation dispersion about the estimated principal axis.
+Normalized to be between 0 (parallel fibers) and 1 (random orientation).
+
+FIT_ISOVF.nii.gz - Isotropic volume fraction. This is the estimated fraction of
+the voxel that is occupied by CSF.
+
+FIT_dir.nii.gz - Vector image containing the estimated principal neurite axis in
+each voxel.
+
+config.pickle - serialized object produced by AMICO.
+
+## Citations
+
+Accelerated Microstructure Imaging via Convex Optimization (AMICO) from diffusion MRI data Alessandro Daducci, Erick Canales-Rodriguez, Hui Zhang, Tim Dyrby, Daniel C Alexander, Jean-Philippe Thiran NeuroImage 105, pp. 32-44 (2015)
+
+NODDI: practical in vivo neurite orientation dispersion and density imaging of the human brain Hui Zhang, Torben Schneider, Claudia A Wheeler-Kingshott, Daniel C Alexander NeuroImage. 16;61(4):1000-16 (2012)
+
+
+
+## Container source
+
+https://github.com/cookpa/amico-noddi
---
+++ bin/runAmicoNoddi.sh
@@ -0,0 +1,196 @@
+#!/bin/bash -e
+
+cleanup=1
+
+scriptPath=$(readlink -f "$0")
+scriptDir=$(dirname "${scriptPath}")
+# Repo base dir under which we find bin/ and containers/
+repoDir=${scriptDir%/bin}
+
+function usage() {
+  echo "Usage:
+  $0 [-h] [-c 1/0] \\
+    -v containerVersion -i /path/to/data/dwi -m /path/to/mask.nii.gz -o /path/to/output/outputFilePrefix [ -- [container args] ]
+
+  Additional args may be passed to the container after terminating script args with '--'.
+"
+}
+
+function help() {
+    usage
+  echo "
+
+Required args:
+
+  -i
+    Input absolute path to DWI data where /path/to/data/dwi.[nii.gz, bval, bvec] exists.
+
+  -m
+    Brain mask image.
+
+  -o
+    Output absolute path on the local file system, including file prefix.
+
+  -v version
+     container version to run. The script will look for containers/amico-noddi-[version].sif.
+
+
+Options:
+
+  -c 1/0
+     Cleanup the working dir after running the prep (default = $cleanup).
+
+  -h
+     Prints this help message.
+
+
+Output:
+
+NODDI metrics computed via AMICO, and a pickle file produced by AMICO:
+
+  * FIT_ICVF.nii.gz
+  * FIT_OD.nii.gz
+  * FIT_ISOVF.nii.gz
+  * FIT_dir.nii.gz
+  * config.pickle
+
+See README or container website for more information and citations
+
+https://github.com/cookpa/amico-noddi
+
+"
+}
+
+if [[ $# -eq 0 ]]; then
+  usage
+  exit 1
+fi
+
+containerVersion=""
+inputRoot=""
+mask=""
+outputRoot=""
+
+while getopts "c:i:m:o:v:h" opt; do
+  case $opt in
+    c) cleanup=$OPTARG;;
+    h) help; exit 1;;
+    i) inputRoot=$OPTARG;;
+    m) mask=$OPTARG;;
+    o) outputRoot=$OPTARG;;
+    v) containerVersion=$OPTARG;;
+    \?) echo "Unknown option $OPTARG"; exit 2;;
+    :) echo "Option $OPTARG requires an argument"; exit 2;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+amicoUserArgs="$*"
+
+image="${repoDir}/containers/amico-noddi-${containerVersion}.sif"
+
+if [[ ! -f "$image" ]]; then
+  echo "Cannot find requested container $image"
+  exit 1
+fi
+
+if [[ -z "${LSB_JOBID}" ]]; then
+  echo "This script must be run within a (batch or interactive) LSF job"
+  exit 1
+fi
+
+sngl=$( which singularity ) ||
+    ( echo "Cannot find singularity executable. Try module load DEV/singularity"; exit 1 )
+
+if [[ ! -f "${inputRoot}.nii.gz" ]]; then
+  echo "Cannot find DWI data ${inputRoot}.nii.gz"
+  exit 1
+fi
+
+# Get input dir to mount into the container as /data/dwi
+inputDir=$(dirname "$(readlink -f "${inputRoot}")")
+inputFileRoot=$(basename "$(readlink -f "${inputRoot}")")
+
+if [[ ! -f "${mask}" ]]; then
+  echo "Cannot find brain mask $mask"
+  exit 1
+fi
+
+maskDir=$(dirname "$(readlink -e "${mask}")")
+maskFile=$(basename "$(readlink -e "${mask}")")
+
+outputDir=$(dirname "$(readlink -m "${outputRoot}")")
+outputFileRoot=$(basename "$(readlink -m "${outputRoot}")")
+
+if [[ ! -d "${outputDir}" ]]; then
+  mkdir -p "$outputDir"
+fi
+
+if [[ ! -d "${outputDir}" ]]; then
+  echo "Could not find or create output directory ${outputDir}"
+  exit 1
+fi
+
+# Set a job-specific temp dir
+jobTmpDir=$( mktemp -d -p ${SINGULARITY_TMPDIR} amicoNODDI.${LSB_JOBID}.XXXXXXXX.tmpdir ) ||
+    ( echo "Could not create job temp dir ${jobTmpDir}"; exit 1 )
+
+# module DEV/singularity sets SINGULARITYENV_TMPDIR=/scratch
+# We make a temp dir there and bind to /tmp in the container
+export SINGULARITYENV_TMPDIR="/tmp"
+
+# singularity args
+singularityArgs="--cleanenv \
+  -B ${jobTmpDir}:/tmp \
+  -B ${inputDir}:/data/input \
+  -B ${maskDir}:/data/mask \
+  -B ${outputDir}:/data/output"
+
+echo "
+--- Script options ---
+Container image        : $image
+Input DWI prefix       : $inputRoot
+Brain mask             : $mask
+Output prefix          : $outputRoot
+Cleanup temp           : $cleanup
+---
+"
+
+echo "
+--- Container details ---"
+singularity inspect $image
+echo "---
+"
+
+cmd="singularity run \
+  $singularityArgs \
+  $image \
+  --dwi-root /data/input/${inputFileRoot} \
+  --brain-mask /data/mask/${maskFile} \
+  --output-root /data/output/${outputFileRoot} \
+  ${amicoUserArgs}
+  "
+
+echo "
+--- amico-noddi command ---
+$cmd
+---
+"
+
+($cmd)
+singExit=$?
+
+if [[ $singExit -ne 0 ]]; then
+  echo "Container exited with non-zero code $singExit - this indicates a problem in the call to"
+  echo "'singularity run', such as invalid disk bindings"
+fi
+
+if [[ $cleanup -eq 1 ]]; then
+  echo "Removing temp dir ${jobTmpDir}"
+  rm -rf ${jobTmpDir}
+else
+  echo "Leaving temp dir ${jobTmpDir}"
+fi
+
+exit $singExit
